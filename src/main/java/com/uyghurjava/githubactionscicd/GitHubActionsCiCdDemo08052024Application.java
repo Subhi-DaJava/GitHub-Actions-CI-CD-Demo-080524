818284f805eeddf71bbcccd740671f311f5a0c32
Subhi YD <subhy809fr@gmail.com>
2024-05-08 13:12:46 +0200

    create GitHub Actions CI/CD demo project

---
+++ src/main/java/com/uyghurjava/githubactionscicd/GitHubActionsCiCdDemo08052024Application.java
@@ -0,0 +1,13 @@
+package com.uyghurjava.githubactionscicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitHubActionsCiCdDemo08052024Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitHubActionsCiCdDemo08052024Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/uyghurjava/githubactionscicd/GitHubActionsCiCdDemo08052024ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uyghurjava.githubactionscicd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitHubActionsCiCdDemo08052024ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
